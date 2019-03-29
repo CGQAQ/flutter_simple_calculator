4e82a3fa4b54976075bc0f362cc3aa6bec826c88
Jason <m.jason.liu@outlook.com>
2019-03-29 11:59:35 +0800

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:demo/calculator.dart';
 
 void main() => runApp(MyApp());
 
@@ -36,21 +37,51 @@ enum Opareator {
   Equals,
 }
 
+enum EState{
+  I,  // can input
+  B,  // can't do anything
+  E   // got result, can change to I to input, cannot change to B
+}
+
 class _MainState extends State<_Main> {
   List<Opareator> _ops = [];
   List<double> _nums = [];
   double _result = 0.0;
   String _str = '';
-  String _temp = '';
+
+  EState _eState = EState.I;
 
 
   RaisedButton numpad(String text, VoidCallback onpress) {
     return RaisedButton(
-      onPressed: onpress,
+      onPressed: _eState == EState.B ? null : onpress,
       child: Text(text),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
       padding: EdgeInsets.all(20),
     );
+  }
+
+  insert(String s){
+    setState(() {
+      if(_eState ==EState.E){
+        _str = '';
+        _eState =EState.I;
+      }
+      _str = '$_str$s';
+    });
+  }
+
+  equal(){
+    var expression = _str;
+    setState(() {
+      _eState =EState.B;
+      try {
+        _str = 'Result: ${Calculator.parse(expression).getResult()}';
+      } catch (e) {
+        _str = 'Error!';
+      }
+      _eState = EState.E;
+    });
   }
 
   Widget build(context) {
@@ -61,7 +92,12 @@ class _MainState extends State<_Main> {
             child: SizedBox.expand(
               child: Card(
                   child: Center(
-                    child: Text(_str),
+                    child: Text(
+                      _str,
+                      style: TextStyle(
+                        fontSize: _eState == EState.I ? 20 : 40
+                      ),
+                    ),
                     widthFactor: 1,
                     heightFactor: 2,
                   ),
@@ -80,13 +116,19 @@ class _MainState extends State<_Main> {
                           numpad('AC', (){
                             setState(() {
                               _str = '';
-                              _temp = '';
-
                             });
                           }),
-                          numpad('DEL', (){}),
-                          numpad('^', (){}),
-                          numpad('+', (){}),
+                          numpad('DEL', (){
+                            setState(() {
+                              _str = _str.substring(0, _str.length - 1);
+                            });
+                          }),
+                          numpad('^', (){
+                            insert('^');
+                          }),
+                          numpad('+', (){
+                            insert('+');
+                          }),
                         ],
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       ),
@@ -95,10 +137,18 @@ class _MainState extends State<_Main> {
                     Container(
                       child: Row(
                         children: <Widget>[
-                          numpad('1', (){}),
-                          numpad('2', (){}),
-                          numpad('3', (){}),
-                          numpad('-', (){}),
+                          numpad('1', (){
+                            insert('1');
+                          }),
+                          numpad('2', (){
+                            insert('2');
+                          }),
+                          numpad('3', (){
+                            insert('3');
+                          }),
+                          numpad('-', (){
+                            insert('-');
+                          }),
                         ],
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       ),
@@ -107,10 +157,18 @@ class _MainState extends State<_Main> {
                     Container(
                       child: Row(
                         children: <Widget>[
-                          numpad('4', (){}),
-                          numpad('5', (){}),
-                          numpad('6', (){}),
-                          numpad('x', (){}),
+                          numpad('4', (){
+                            insert('4');
+                          }),
+                          numpad('5', (){
+                            insert('5');
+                          }),
+                          numpad('6', (){
+                            insert('6');
+                          }),
+                          numpad('*', (){
+                            insert('*');
+                          }),
                         ],
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       ),
@@ -119,10 +177,18 @@ class _MainState extends State<_Main> {
                     Container(
                       child: Row(
                         children: <Widget>[
-                          numpad('7', (){}),
-                          numpad('8', (){}),
-                          numpad('9', (){}),
-                          numpad('/', (){}),
+                          numpad('7', (){
+                            insert('7');
+                          }),
+                          numpad('8', (){
+                            insert('8');
+                          }),
+                          numpad('9', (){
+                            insert('9');
+                          }),
+                          numpad('/', (){
+                            insert('/');
+                          }),
                         ],
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       ),
@@ -131,9 +197,15 @@ class _MainState extends State<_Main> {
                     Container(
                       child: Row(
                         children: <Widget>[
-                          numpad('.', (){}),
-                          numpad('0', (){}),
-                          Expanded(child: numpad('=', (){})),
+                          numpad('.', (){
+                            insert('.');
+                          }),
+                          numpad('0', (){
+                            insert('0');
+                          }),
+                          Expanded(child: numpad('=', (){
+                            equal();
+                          })),
                         ],
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       ),
